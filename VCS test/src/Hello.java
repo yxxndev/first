db3793f81d036587971ae78cd5a52f73a7355e30
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-25 10:09:03 +0900

    첫번째

--- VCS test/src/Hello.java
+++ VCS test/src/Hello.java
@@ -1,8 +1,12 @@
 public class Hello {
 	public static void main(String[] args) {
+		// 로컬에서 작업했습니다.
 		System.out.println("Hello world");
 		
+		System.out.println("변경");
+	
+		System.out.println("세번째 커밋");
 		
-		System.out.println("¼¼¹øÂ° Ä¿¹Ô");
+		// 주석
 	}
 }
